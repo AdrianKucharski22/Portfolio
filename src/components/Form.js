ec37a1fba600a2b1b9104bfde7f8a220eed07520
Adrian Kucharski <AdrianKucharski22@gmail.com>
2022-09-19 22:01:57 +0200

    better version of validaction

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,54 +1,107 @@
+import React from "react";
 function Form(){
-    function check(){
-        const userName = document.querySelector('.userName')
-        const nameError = document.querySelector('.nameError')
-        const userEmail = document.querySelector('.userEmail')
-        const emailError = document.querySelector('.emailError')
-        const userMessage = document.querySelector('.userMessage')
-        const messageError = document.querySelector('.messageError')
-        if (userName.value.length < 3 || userName.value.includes(" ") === true || userName.value === ""){
-            nameError.textContent = "imię musi być dłuższe niż 3 litery"
+        const [error,setError] = React.useState(null);
+        const [form, setForm] = React.useState({
+            name:'',
+            email:'',
+            message:''
+        })
+        const handleSubmit = async (e) =>{
+            e.preventDefault()
+            const errorMsg = validate(form)
+            if (errorMsg){
+                setError(errorMsg)
+                console.log('błąd')
+                return
+            }
+            console.log('formSubmited',form)
         }
-        else{
-            nameError.textContent = ""
+        const updateField = e =>{
+            setForm({
+                ...form,
+                [e.target.name]: e.target.value
+            })
         }
-        if (userEmail.value.includes('@') === false || userEmail.value === ""){
-            console.log("brak");
-            emailError.textContent = "brak znaków specjalnych"
+
+        const validate = form =>{
+            if (!form.email){
+            return 'Email jest wymagany'
         }
-        else {
-            console.log('posiada')
-            emailError.textContent = ""
+            else if (! /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/i.test(form.email)){
+                return 'Zły e-mail'
+            }
+
+            if (!form.name){
+                return "Imię jest wymagane"
+            }
+            else if(form.name.length < 2 || form.name.value.includes(" ") === true){
+                return "imię jest za krótkie bądź posiada spacje"
+            }
+
+            if (!form.message){
+                return "wiadomość jest wymagana"
+            }
+            else if (form.message.length < 120){
+                return "wiadomość jest za krótka"
+            }
+            else{
+                return ""
+            }
+
+            return null
         }
-        if (userMessage.value.length < 120 || userMessage.value === ""){
-            console.log('zamało liter')
-            messageError.textContent = "Wiadomośc powinna mieć conajmniej 120 znaków"
-        }
-        else{
-            console.log('odpowiednia długośc')
-            messageError.textContent = ""
-        }
-    }
+
+    // function check(){
+    //     const userName = document.querySelector('.userName')
+    //     const nameError = document.querySelector('.nameError')
+    //     const userEmail = document.querySelector('.userEmail')
+    //     const emailError = document.querySelector('.emailError')
+    //     const userMessage = document.querySelector('.userMessage')
+    //     const messageError = document.querySelector('.messageError')
+    //     if (userName.value.length < 3 || userName.value.includes(" ") === true || userName.value === ""){
+    //         nameError.textContent = "imię musi być dłuższe niż 3 litery"
+    //     }
+    //     else{
+    //         nameError.textContent = ""
+    //     }
+    //     if (userEmail.value.includes('@') === false || userEmail.value === ""){
+    //         console.log("brak");
+    //         emailError.textContent = "brak znaków specjalnych"
+    //     }
+    //     else {
+    //         console.log('posiada')
+    //         emailError.textContent = ""
+    //     }
+    //     if (userMessage.value.length < 120 || userMessage.value === ""){
+    //         console.log('zamało liter')
+    //         messageError.textContent = "Wiadomośc powinna mieć conajmniej 120 znaków"
+    //     }
+    //     else{
+    //         console.log('odpowiednia długośc')
+    //         messageError.textContent = ""
+    //     }
+    // }
     return(
-        <form className={'myForm'} >
+        <form className={'myForm'}>
+            {error}
             <div className={"yourData"}>
                 <div className="name">
                     <p>Wpisz swoje imię</p>
-                    <input type={'text'} name={"userName"} className={'userName'} placeholder={'Imię'} />
+                    <input type={'text'} name={"userName"} className={'userName'} placeholder={'Imię'} name={'name'} id={'name'} onChange={updateField}/>
                     <div className={'nameError error'}></div>
                 </div>
                 <div className="email">
                     <p>Wpisz swoje email</p>
-                    <input type={'email'} className={'userEmail'} placeholder={'abc@xyz.pl'}/>
+                    <input type={'email'} className={'userEmail'} placeholder={'abc@xyz.pl'} name={'email'} id={'email'} onChange={updateField}/>
                     <div className={'emailError error'}></div>
                 </div>
             </div>
             <div className="message">
                 <p>Wpisz swoją wiadomość</p>
-                <textarea placeholder={'Treść wiadomości'} className={'userMessage'}/>
+                <textarea placeholder={'Treść wiadomości'} className={'userMessage'} name={'message'} id={'message'} onChange={updateField}/>
                 <div className={'messageError error'}></div>
             </div>
-            <button onClick={check}>Wyślij</button>
+            <button  onClick={handleSubmit}>Wyślij</button>
         </form>
         )
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -2,25 +2,64 @@ import {Link} from "react-router-dom";
 import Nav from "./Nav";
 import Decoration from "../assets/Decoration.svg";
 import '../scss/main.scss'
+import React from "react";
 function Login(){
-    function check(){
-        const email = document.querySelector('.email')
-        const emailError = document.querySelector('.emailError')
-        const password = document.querySelector('.password')
-        const passwordError = document.querySelector('.passwordError')
-        if (email.value.includes('@') === false || email.value.includes(' ') === true){
-            emailError.textContent="błędny email"
+    const [error,setError] = React.useState(null);
+    const [form, setForm] = React.useState({
+        email:'',
+        password:''
+    })
+    const handleSubmit = async (e) =>{
+        e.preventDefault()
+        const errorMsg = validate(form)
+        if (errorMsg){
+            setError(errorMsg)
+            console.log('błąd')
+            return
         }
-        else {
-            emailError.textContent=""
-        }
-        if (password.value.length < 6){
-            passwordError.textContent = "Hasło musi mieć conajmniej 6 znakow"
-        }
-        else {
-            passwordError.textContent = ""
-        }
+        console.log('formSubmited',form)
     }
+    const updateField = e =>{
+        setForm({
+            ...form,
+            [e.target.name]: e.target.value
+        })
+    }
+    const validate = form =>{
+        if (!form.email){
+            return 'Email jest wymagany'
+        }
+        else if (! /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/i.test(form.email)){
+            return 'Zły e-mail'
+        }
+
+        if (!form.password){
+            return "Hasło jest wymagane"
+        }
+        else if (form.password.length < 6){
+            return "hasło musi być dłuższe niż 6 liter"
+        }
+
+        return null
+    }
+    // function check(){
+    //     const email = document.querySelector('.email')
+    //     const emailError = document.querySelector('.emailError')
+    //     const password = document.querySelector('.password')
+    //     const passwordError = document.querySelector('.passwordError')
+    //     if (email.value.includes('@') === false || email.value.includes(' ') === true){
+    //         emailError.textContent="błędny email"
+    //     }
+    //     else {
+    //         emailError.textContent=""
+    //     }
+    //     if (password.value.length < 6){
+    //         passwordError.textContent = "Hasło musi mieć conajmniej 6 znakow"
+    //     }
+    //     else {
+    //         passwordError.textContent = ""
+    //     }
+    // }
     return(
         <div className={'Login'}>
             <Nav/>
@@ -28,16 +67,17 @@ function Login(){
                 <h1>Zaloguj się</h1>
                 <img src={Decoration} alt={''}/>
                 <form>
+                    <p className={'error'}>{error}</p>
                     <p>Email</p>
-                    <input type={"email"} className={'email'}/>
+                    <input type={"email"} className={'email'} id='email' name='email' onChange={updateField}/>
                     <p className={'emailError error'}></p>
                     <p>Hasło</p>
-                    <input type={"password"} className={'password'}/>
+                    <input type={"password"} className={'password'} id='password' name='password' onChange={updateField}/>
                     <p className={'passwordError error'}></p>
                 </form>
                 <div className={'buttons'}>
                     <button><Link to={'/Register'} alt={'Register'}>Załóż konto</Link></button>
-                    <button onClick={check}>Zaloguj się</button>
+                    <button onClick={handleSubmit}>Zaloguj się</button>
                 </div>
             </article>
 
